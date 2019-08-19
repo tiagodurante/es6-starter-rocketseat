04ea647701e7c3ec54c521bbe1acce600d38fb75
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-19 15:40:33 -0300

    trabalhando com classes

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-alert("uhuu");
+class List {
+  constructor() {
+    this.data = [];
+  }
 
-class Teste {
-  metodo() {}
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = "Tiago";
+  }
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+const MinhaLista = new TodoList();
+document.getElementById("novotodo").onclick = () => {
+  MinhaLista.add("novo");
+  MinhaLista.mostraUsuario();
+};
