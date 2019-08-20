2234e829bc6561f70efd5fc8d233f5eaf8b47bcc
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-20 20:25:15 -0300

    valores padrão

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-const arr = [1, 2, 3, 4, 5, 6, 8];
+const soma = (a = 3, b = 6) => a + b;
 
-const newArr = arr.map((item, index) => item * 2);
-
-console.log(newArr);
-
-const sum = arr.reduce((total, next) => total + next);
-
-console.log(sum);
-
-const filter = arr.filter(item => item % 2 === 0);
-
-console.log(filter);
-
-const find = arr.find(item => item === 7);
-
-console.log(find);
-
-const teste = () => ({ nome: "tiago" });
-
-console.log(teste());
+console.log(soma(1));
+console.log(soma());
